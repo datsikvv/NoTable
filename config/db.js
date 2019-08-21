beed5bba66fd19e3baff13044f1a3850f044b5d9
Volodymyr <31970034+datsikvv@users.noreply.github.com>
2019-08-22 01:17:36 +0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url : "mongodb+srv://Volodymyr:Rakicka32@cluster0-b7vkx.mongodb.net/test?retryWrites=true&w=majority"
+    //url : "mongodb+srv://Volodymyr:R*******2@cluster0-b7vkx.mongodb.net/test?retryWrites=true&w=majority"
+  	url : "mongodb://localhost/notable"
   };
